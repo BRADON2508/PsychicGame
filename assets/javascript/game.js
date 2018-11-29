454933ec6ddda34c42d53eda3df02c2a1cddc22b
Brandon <mgsbigboss85@live.com>
2018-11-29 17:37:30 -0500

    finished all homework files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,47 @@
 // var userChoices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,];
-var input = document.createElement("INPUT");
-input.setAttribute("type", "number");
-var userGuess = document.getElementById("input");
+
+var userinput = document.getElementById("input");
 var computerChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
 var correct = 0;
 var wrong = 0;
+var userGuess;
 var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var computerGuess =document.getElementById("computerGuess-text");
+var userGuessText = document.getElementById("userGuess-text");
+var computerGuessText = document.getElementById("computerGuess-text");
 var correctText = document.getElementById("correct-text");
 var wrongText = document.getElementById("wrong-text");
+var button = document.querySelector("button");
+
 document.onkeyup = function(event) {
     var userChoice = event.key;
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
-if (userGuess === computerGuess) {
-    correct++;
-} else if (userGuess == computerGuess) {
-    wrong++;
+
+// for (var i = 0; i < numbers.length; i++) {
+    // var numberBtn = $("<button>");
+    // numberBtn.addClass("number-button number number-button-color");
+    // numberBtn.text(numbers[i]);
+    // $("#buttons").append(numberBtn);
+// }
+button.onclick = function(event) {
+    event.preventDefault();
+    userGuess = input.value;
+    input.value = "";
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // computerGuess = 3;
+    console.log(userGuess);
+    console.log(computerGuess);
+    if (userGuess == computerGuess) {
+        correct++;
+        correctText.innerHTML = "Correct: " + correct;
+        
+    } else {
+        wrong++;
+        wrongText.innerHTML = "Wrong: " + wrong;
+    }
+userGuessText.innerHTML = "You guessed: " + userGuess;
+computerGuessText.innerHTML = "The Computer chose: " + computerGuess;
+// correctText.textContent = "You Win!!! You have won: " + correct;
+// wrongText.textContent = "You failed to guess the number. You have lost: " + wrong;
+
 }
-userChoiceText.textContent = "You guessed: " + userGuess;
-computerChoiceText.textContent = "The Computer chose: " + computerGuess;
-correctText.textContent = "You Win!!! You have won: " + correct;
-wrongText.textContent = "You failed to guess the number. You have lost: " + wrong;
